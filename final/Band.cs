723a0cf0582c2fbd6c1ec254988ada8c41aebc62
Nathan Lavin <nathan_lavin@insideranken.org>
2023-05-02 09:10:57 -0500

    p

--- final/Band.cs
+++ final/Band.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +16,7 @@ namespace final
 
         public Band(string name)
         {
-            Name = name;
+            _name = name;
         }
 
         public string Name 
